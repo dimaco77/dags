07259f056c3eeccfb4ffb85f062f4f89d93331f5
Gonza L <gonzalo.lucena@accenture.com>
2021-06-28 10:52:53 -0300

    Update dag_execute_azure_databricks.py

--- dag_execute_azure_databricks.py
+++ dag_execute_azure_databricks.py
@@ -12,6 +12,7 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.providers.databricks.hooks.databricks import DatabricksHook
 from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
+from airflow.contrib.operators.databricks_operator import DatabricksRunNowOperator
 
 default_args = {
     'owner': 'accenture',
@@ -38,7 +39,6 @@ hook = DatabricksHook(databricks_conn_id)
 #hook.run_pipeline(pipeline_name,resource_group_name='RG-TDP-TDL-DEV',factory_name='dftdptdldev-core01')
 
 
-
 # DAG
 with DAG(dag_id='dag_execute_azure_databricks',
 		 default_args=default_args,
@@ -48,8 +48,25 @@ with DAG(dag_id='dag_execute_azure_databricks',
 	#TASK 1
 	start = DummyOperator(task_id = 'start')
 
+    #TASK 3
+        spark_jar_task = DatabricksSubmitRunOperator(
+    task_id='spark_jar_task',
+    dag=dag,
+    new_cluster=new_cluster,
+    spark_jar_task={
+        'main_class_name': 'com.example.ProcessData'
+    },
+    libraries=[
+        {
+            'jar': 'dbfs:/lib/etl-0.1.jar'
+        }
+    ]
+    )
+
 	#TASK 2
 	end = DummyOperator(task_id = 'end')
+
+
 
 
 start >> end
