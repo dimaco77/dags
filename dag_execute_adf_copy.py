871ce0ebd1b15c2fca2be830c69e8cdd61bb6d98
dimaco77 <86628846+dimaco77@users.noreply.github.com>
2021-06-28 17:27:54 -0300

    Update dag_execute_adf_copy.py

--- dag_execute_adf_copy.py
+++ dag_execute_adf_copy.py
@@ -5,20 +5,41 @@ from airflow.providers.microsoft.azure.hooks.azure_data_factory import AzureData
 
 azure_data_factory_conn = 'azure_data_factory_conn'
 
+#Get yesterday's date, in the correct format
+yesterday_date = '{{ yesterday_ds_nodash }}'
+
 def run_adf_pipeline(pipeline_name, date):
     '''Runs an Azure Data Factory pipeline using the AzureDataFactoryHook and passes in a date parameter
     '''
+
+    #Create a dictionary with date parameter
+    params = {}
+    params["date"] = date
 
     #Make connection to ADF, and run pipeline with parameter
     hook = AzureDataFactoryHook(azure_data_factory_conn)
     hook.run_pipeline(pipeline_name, parameters=params)
 
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5)
+}
+
 with DAG(
     'azure_data_factory',
-  ) as dag:
+    start_date=datetime(2019, 1, 1),
+    max_active_runs=1,
+    schedule_interval=timedelta(minutes=30),
+    default_args=default_args,
+    catchup=False
+) as dag:
 
     opr_run_pipeline = PythonOperator(
         task_id='run_pipeline',
         python_callable=run_adf_pipeline,
-        op_kwargs={'pipeline_name': 'Orchestration_ps_ts_generic_datasets_dataQuality'}
+        op_kwargs={'pipeline_name': 'Orchestration_ps_ts_generic_datasets_dataQuality', 'date': yesterday_date}
     )
