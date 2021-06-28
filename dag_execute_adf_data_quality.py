7548fa18bb5a95403d0840dbf731477f736e26f5
Emanuel Baquero <48158508+emanuelbaquero@users.noreply.github.com>
2021-06-28 19:29:58 -0300

    actu librarys

--- dag_execute_adf_data_quality.py
+++ dag_execute_adf_data_quality.py
@@ -32,12 +32,55 @@ def run_adf_pipeline(pipeline_name):
 
     #Create a dictionary with date parameter
     params = {}
-
-    #Make connection to ADF, and run pipeline with parameter
+    v_resource_group_name='RG-TDP-TDL-DEV'
+    v_factory_name='dftdptdldev-core01'
+    v_linked_service='ls_adls_tdl'
+    v_dataset_name='prueba_parquet'
     hook = AzureDataFactoryHook(azure_data_factory_conn_id)
-    print('NOMBRE DEL PIPELINE: ', hook.get_pipeline(pipeline_name=pipeline_name,resource_group_name='RG-TDP-TDL-DEV',factory_name='dftdptdldev-core01'))
-    #hook.run_pipeline(pipeline_name,resource_group_name='RG-TDP-TDL-DEV',factory_name='dftdptdldev-core01')
-    
+
+    try:
+        #Make connection to ADF, and run pipeline with parameter
+        print('NOMBRE DEL PIPELINE: ', hook.get_factory(pipeline_name=pipeline_name,
+                                                        resource_group_name=v_resource_group_name,
+                                                        factory_name=v_factory_name))
+    except:
+        print('Fallo get_factory')
+        
+        
+    try:
+        if hook._factory_exists(v_resource_group_name,v_factory_name):
+            print('Existe ADF')
+        else:
+            print('No Existe ADF')
+    except:
+        print('Fallo _factory_exists')
+        
+        
+    try:
+        hook.get_linked_service(v_linked_service,v_resource_group_name,v_factory_name)
+    except:
+        print('Fallo get_linked_service')
+        
+
+    try:
+        if hook._linked_service_exists(v_resource_group_name,v_factory_name,v_linked_service):
+            print('Existe Linked Service ,v_linked_service)
+        else:
+            print('No Existe Linked Service, v_linked_service)
+    except:
+        print('Fallo _linked_service_exists')
+                  
+                  
+    try:
+        hook.get_dataset(v_dataset_name,v_resource_group_name,v_factory_name):
+    except:
+        print('Fallo _linked_service_exists')
+                  
+
+                  
+                  
+            
+        
 
 
 
