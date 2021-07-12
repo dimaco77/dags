1f7fdb02bbd0a5f2ae0c025068b0d5ca785b5116
Gonza L <gonzalo.lucena@accenture.com>
2021-07-12 11:31:15 -0300

    Update dag__demo_vars.py

--- dag__demo_vars.py
+++ dag__demo_vars.py
@@ -57,103 +57,103 @@ def chau_world_loop():
 
 
 def run_adf_pipeline(pipeline_name):
-    print("----------run_pipeline var")
-    print(tof_pipeline_run)
-    print("----------run_all var")
-    print(tof_az_func)
-    print("----------run_func var") 
-    print(tof_all)
-    print("----------types")
-    print(type(tof_pipeline_run))
-    print(type(tof_az_func))
-    print(type(tof_all))
-
     if tof_pipeline_run.lower() != "true" or tof_all.lower() != "true":
-        return "False"
+        print("----------run_pipeline var")
+        print(tof_pipeline_run)
+        print("----------run_all var")
+        print(tof_az_func)
+        print("----------run_func var") 
+        print(tof_all)
+        print("----------types")
+        print(type(tof_pipeline_run))
+        print(type(tof_az_func))
+        print(type(tof_all))
 
-    '''Runs an Azure Data Factory pipeline using the AzureDataFactoryHook and passes in a date parameter
-    '''
+        '''Runs an Azure Data Factory pipeline using the AzureDataFactoryHook and passes in a date parameter
+        '''
 
-    #Create a dictionary with date parameter
-    params = {}
-    v_resource_group_name = Variable.get("resource_group_name")
-    v_factory_name = Variable.get("factory_name")
-    v_linked_service = Variable.get("linked_service")
-    v_dataset_name = Variable.get("dataset_name")
-    hook = AzureDataFactoryHook(azure_data_factory_conn_id)
+        #Create a dictionary with date parameter
+        params = {}
+        v_resource_group_name = Variable.get("resource_group_name")
+        v_factory_name = Variable.get("factory_name")
+        v_linked_service = Variable.get("linked_service")
+        v_dataset_name = Variable.get("dataset_name")
+        hook = AzureDataFactoryHook(azure_data_factory_conn_id)
 
-    try:
-        #Make connection to ADF, and run pipeline with parameter
-        print('NOMBRE DEL PIPELINE: ', hook.get_factory(pipeline_name=pipeline_name,
-                                                        resource_group_name=v_resource_group_name,
-                                                        factory_name=v_factory_name))
-        print('----------get_factory funciono Correctamente')
-    except:
-        print('----------Fallo get_factory')
-        
-        
-    try:
-        if hook._factory_exists(v_resource_group_name,v_factory_name):
-            print('----------Se ha verificado la existencia de la ADF correctamente')
-        else:
-            print('----------No Existe ADF')
-    except:
-        print('----------Fallo _factory_exists')
-        
-        
-    try:
-        hook.get_linked_service(v_linked_service,v_resource_group_name,v_factory_name)
-        print('----------get_linked_service funciono Correctamente')
-    except:
-        print('----------Fallo get_linked_service')
-        
+        try:
+            #Make connection to ADF, and run pipeline with parameter
+            print('NOMBRE DEL PIPELINE: ', hook.get_factory(pipeline_name=pipeline_name,
+                                                            resource_group_name=v_resource_group_name,
+                                                            factory_name=v_factory_name))
+            print('----------get_factory funciono Correctamente')
+        except:
+            print('----------Fallo get_factory')
+            
+            
+        try:
+            if hook._factory_exists(v_resource_group_name,v_factory_name):
+                print('----------Se ha verificado la existencia de la ADF correctamente')
+            else:
+                print('----------No Existe ADF')
+        except:
+            print('----------Fallo _factory_exists')
+            
+            
+        try:
+            hook.get_linked_service(v_linked_service,v_resource_group_name,v_factory_name)
+            print('----------get_linked_service funciono Correctamente')
+        except:
+            print('----------Fallo get_linked_service')
+            
 
-    try:
-        if hook._linked_service_exists(v_resource_group_name,v_factory_name,v_linked_service):
-            print('----------Existe Linked Service', v_linked_service)
-        else:
-            print('----------No Existe Linked Service', v_linked_service)
-    except:
-        print('----------Fallo _linked_service_exists')
-                  
-                  
-    try:
-        hook.get_dataset(v_dataset_name,v_resource_group_name,v_factory_name)
-        print('----------get_dataset funciono Correctamente')
-    except:
-        print('----------Fallo get_dataset')
-                  
+        try:
+            if hook._linked_service_exists(v_resource_group_name,v_factory_name,v_linked_service):
+                print('----------Existe Linked Service', v_linked_service)
+            else:
+                print('----------No Existe Linked Service', v_linked_service)
+        except:
+            print('----------Fallo _linked_service_exists')
+                    
+                    
+        try:
+            hook.get_dataset(v_dataset_name,v_resource_group_name,v_factory_name)
+            print('----------get_dataset funciono Correctamente')
+        except:
+            print('----------Fallo get_dataset')
+                    
 
-    try:
-        if hook._dataset_exists(v_resource_group_name,v_factory_name,v_dataset_name):
-            print('----------Existe Dataset' ,v_dataset_name)
-        else:
-            print('----------No Existe Dataset', v_dataset_name)
-    except:
-        print('----------Fallo _dataset_exists')
+        try:
+            if hook._dataset_exists(v_resource_group_name,v_factory_name,v_dataset_name):
+                print('----------Existe Dataset' ,v_dataset_name)
+            else:
+                print('----------No Existe Dataset', v_dataset_name)
+        except:
+            print('----------Fallo _dataset_exists')
 
-    try:
-        hook.get_pipeline(pipeline_name, v_resource_group_name, v_factory_name)
-        print('----------get_pipeline funciono Correctamente')
-    except:
-        print('----------Fallo get_dataset')
-
-
-
-    try:
-        if hook._pipeline_exists(v_resource_group_name,v_factory_name,pipeline_name):
-            print('----------Existe Pipeline' ,pipeline_name)
-        else:
-            print('----------No Existe Pipeline', pipeline_name)
-    except:
-        print('----------Fallo _pipeline_exists')
+        try:
+            hook.get_pipeline(pipeline_name, v_resource_group_name, v_factory_name)
+            print('----------get_pipeline funciono Correctamente')
+        except:
+            print('----------Fallo get_dataset')
 
 
-    try:
-        hook.run_pipeline(pipeline_name, v_resource_group_name, v_factory_name)
-        print('----------run_pipeline funciono Correctamente')
-    except:
-        print('----------Fallo run_pipeline')
+
+        try:
+            if hook._pipeline_exists(v_resource_group_name,v_factory_name,pipeline_name):
+                print('----------Existe Pipeline' ,pipeline_name)
+            else:
+                print('----------No Existe Pipeline', pipeline_name)
+        except:
+            print('----------Fallo _pipeline_exists')
+
+
+        try:
+            hook.run_pipeline(pipeline_name, v_resource_group_name, v_factory_name)
+            print('----------run_pipeline funciono Correctamente')
+        except:
+            print('----------Fallo run_pipeline')
+    else:
+        print("-----------run var = false")
 
 #IF task fails before retries
 def on_retry_callback(context):
